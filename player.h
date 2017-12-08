1026748dbb78cc5943af1d2d76b4fe34f2493aa2
karenlin7788 <xlin2@mail.csuchico.edu>
2017-12-07 16:47:17 -0800

    player.h

---
+++ player.h
@@ -0,0 +1,21 @@
+#ifndef _STUDENT_H_
+#define _STUDENT_H_
+#include <iostream>
+#include <string>
+
+using std::string;
+class player
+
+{
+private:
+string first;
+string last;
+
+public:
+void setName(string,string);
+string fullName();
+
+
+};
+
+#endif
